97fa9b4d688b0acfc43e8ca220d935234dd28f1d
Nandlal Kamat <38187138+kamatnandlal@users.noreply.github.com>
2025-01-30 23:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,5 @@
-demo
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-unique-bucket-name"
+  location = "US"
+  force_destroy = true  # Allow the bucket to be deleted even if it contains objects
+}
